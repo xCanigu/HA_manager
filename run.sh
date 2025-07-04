54fbd57a013c845a90cd342eb5943cb1ed222350
Pântea Alexandra Carmen <99326543+xCanigu@users.noreply.github.com>
2025-07-04 11:19:01 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/with-contenv bashio
 
-SERVER_URL=$(bashio::config 'server_url')
-TOKEN=$(bashio::config 'token')
-
-python3 /heartbeat_client.py "$SERVER_URL" "$TOKEN"
-        
+echo "Hello world!"
